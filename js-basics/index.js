dc0bbd2abb572af2a13d608f9583c65df738f51d
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-07-18 01:05:33 -0400

    first commit

---
+++ js-basics/index.js
@@ -0,0 +1,19 @@
+// dot notation
+// make a object access different parts and cosnole out.
+// make a function that displays a specific part of an object
+
+
+const numbers = [1,2,3]
+const person ={
+    name: "bob",
+    job: "software"
+}
+
+
+
+function displayIndex(){
+   return person.name
+    
+}
+
+console.log(displayIndex());
