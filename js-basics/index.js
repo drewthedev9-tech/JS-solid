ac2c6f38549ec0ba48e6c1498636a2f74d606e76
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-07-18 01:35:36 -0400

    js-basic practice

--- js-basics/index.js
+++ js-basics/index.js
@@ -25,5 +25,45 @@ let x = 10;
 // same type and value
  console.log( x=== 10) // true
 
- //losse equlatiy only checks value.
+ //losse equlatiy only checks value. in this case 10
  console.log( "10"=== 10) // true 
+
+//  if a customer has more than 100 points.
+// they are a gold customer ? silver customer.
+
+let points = 110;
+let type = points > 100 ? "gold" : silver;
+console.log(type); // gold.
+
+// logical operators ////
+// logical AND &&
+conole.log(true && true )// true
+let hghIncome =false;
+let creditScore = true;
+
+let eligbleForLoan = hghIncome && creditScore;
+console.log(eligbleForLoan); // false
+
+// OR ||
+
+let eligbleForLoan = hghIncome || creditScore;
+console.log(eligbleForLoan); // true
+
+// ! NOT 
+let applicationRefused = eligbleForLoan;
+console.log(eligbleForLoan);//true.
+
+// logical operators with NOn-booleans
+// falsy = undefined, null, 0, false, '', Nan( not a number)
+
+// anything that is not falsy -> truthy = as long as onof the operands is true it will
+// be truthy
+false || "mosh" // mosh
+false || 1 || 2  // 1 once  engine finds a truthy it stops = short circuiting.
+
+
+
+
+
+
+
