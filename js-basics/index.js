21a3498f6dfef49d87768abe3b0f740ac00058cc
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-07-20 13:32:28 -0400

    js-basic practice

--- js-basics/index.js
+++ js-basics/index.js
@@ -3,7 +3,7 @@
 // make a function that displays a specific part of an object
 
 
-const numbers = [1,2,3]
+const numbers1 = [1,2,3]
 const person ={
     name: "bob",
     job: "software"
@@ -37,21 +37,21 @@ console.log(type); // gold.
 
 // logical operators ////
 // logical AND &&
-conole.log(true && true )// true
-let hghIncome =false;
-let creditScore = true;
+// conole.log(true && true )// true
+// let hghIncome =false;
+// let creditScore = true;
 
-let eligbleForLoan = hghIncome && creditScore;
-console.log(eligbleForLoan); // false
+// let eligbleForLoan = hghIncome && creditScore;
+// console.log(eligbleForLoan); // false
 
 // OR ||
 
-let eligbleForLoan = hghIncome || creditScore;
-console.log(eligbleForLoan); // true
+// let eligbleForLoan = hghIncome || creditScore;
+// console.log(eligbleForLoan); // true
 
 // ! NOT 
-let applicationRefused = eligbleForLoan;
-console.log(eligbleForLoan);//true.
+// let applicationRefused = eligbleForLoan;
+// console.log(eligbleForLoan);//true.
 
 // logical operators with NOn-booleans
 // falsy = undefined, null, 0, false, '', Nan( not a number)
@@ -60,6 +60,68 @@ console.log(eligbleForLoan);//true.
 // be truthy
 false || "mosh" // mosh
 false || 1 || 2  // 1 once  engine finds a truthy it stops = short circuiting.
+
+// example application
+let userColor = "red";
+let defaultColor = "blue";
+// if user picks color will be red otherwise defaults to blue.
+// exaple if userColor was "undefined" it would default to blue.
+let currentColor = userColor || defaultColor;
+
+console.log(currentColor);
+
+// control flow
+// hour 6am and 12pm - morning
+// 12pm and 6pm - afternnon
+// otherwise - good evening.
+let hour = 10;
+
+if (hour >= 6 && hour < 12){
+    console.log("good morning")
+} else if( hour >= 12 && hour < 18 ){
+    cosnole.log("good afternoon")
+} else {
+    console.log("good evening")
+}
+
+// switch and case - compare the variable against others, number booleans etc.
+let role = "guest";
+switch (role){
+   case  'guest':
+   console.log('guest User')
+   break;
+
+   case'moderator':
+   console.log("moderator user")
+   break// needs break other statements will be used otherwise.
+
+   default: 
+   console.log("unknown user");
+   break
+}
+
+// for loops ///////
+// index, condition, incrementg expression.
+ const numbers = [1,2,3,4,5];
+
+for( let i = 0; i < numbers.length; i ++){
+    // not an even number
+//    if (i % 2!== 0){
+//        console.log(i); // 1, 3 ,5.
+//    }
+console.log(numbers[i]);
+
+// 
+}
+
+//  do -while 
+let i =0;
+do{
+    if ( i % 2 !== 0) console.log(i);
+    i++
+} while (i = 1 <=5);
+
+
 
 
 
