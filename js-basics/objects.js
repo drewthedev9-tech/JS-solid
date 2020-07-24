b2db9526898ae4b7b8fcb02c3b88f98dc52eca6b
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-07-24 10:55:56 -0400

    js-basic practice

--- js-basics/objects.js
+++ js-basics/objects.js
@@ -52,6 +52,75 @@ const circle = new Cirlce(1);
 
 // functions are objects.
 
+//  enumerating properties of an object.///////
+// for in for objects
+// for of for arrays - BUT using objec.keys or object.entries.
+
+// const circleObj ={
+//     radius: 1, 
+//     draw(){
+//         console.log('draw');
+//     }
+// };
+
+// interating over an object - for in is for objects
+// for of loops are for iterating over arrays.
+for (let key in circleObj){
+    // bracket notationn to get the value of the property.
+    console.log(key, circle[key]);
+}
+
+// way to iterate over object with for of using object.keys.
+for (let key of Object.keys(circleObj))
+    console.log(key);
+
+// way to iterate over object with for of using object.entries.
+for (let entry of Object.entries(circleObj))
+console.log(entry);
+
+// cloning and object////////
+// usig circleObj above.
+
+const circleObj ={
+    radius: 1, 
+    draw(){
+        console.log('draw');
+    }
+};
+
+const another = {
+}
+
+// OLD way of cloning objects
+// for(let key in circleObj)
+//     another[key] = circleObj[Key];
+    // puts circleObj['radius] into anther['radius];
+    // another['radius'] = circleObj['radius'];
+
+// modern javascript way of cloning an object Object.assign
+// can cmbine properties of an object or add.
+// {}= empty object , then is filled circleObj
+// but it doesnt have to be you an overide with specific properties.
+// const another = Object.assign({
+//     color: 'yellow'
+// }, circleObj);
+
+// spread operator way of cloning an object///
+const another = {...circleObj};
+
+// garbage colletor - deallocates the memory of variables that arent being used.
+
+// built in objects and functions.
+// Math - dealing with mathematcial calculations - look at MDN
+
+// String primitive.
+const message ='This is my first message';
+
+// string object
+const another = new String('hi');
+
+
+
 
 
 
